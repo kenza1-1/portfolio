54373656fe99ff201aeb77dbd0a92d303fca56ae
kenza1-1 <iderkenza1@gmail.com>
2020-04-13 12:53:34 +0000

    fisrt commit

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Kenza GUETTAL</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="img/logo.png" />
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/logo.png" alt=""></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#page-top">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#project">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a href="mailto:iderkenza1@gmail.com" target="_blank" class="nav-link text-uppercase font-weight-bold nav-menu"><span><i  class="fas fa-envelope fa-2x  icon-1"></i></span></a>
+          </li>
+          <li class="nav-item">
+            <a href="https://www.facebook.com/profile.php?id=100009352598699" target="_blank" class="nav-link text-uppercase font-weight-bold nav-menu"> <span><i class="fab fa-facebook-f fa-2x "></i></span></a>
+          </li>
+          <li class="nav-item">
+            <a href="https://www.linkedin.com/in/kenza-ider-b91690152/" target="_blank" class="nav-link text-uppercase font-weight-bold nav-menu"> <span><i class="fab fa-linkedin fa-2x "></i></span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-white font-weight-bold"><span class='nearMyname'>Hello, I'm </span><span class='myname'>Kenza Guettal</span><br>
+            <span class='nearMyname'>I am a junior web developer</span></p></h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <!-- <p class="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p> -->
+          <!-- <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a> -->
+          <a href="#projects" class="btn btn-primary btn-xl js-scroll-trigger example_e">View my work</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About me Section -->
+  <section class="page-section" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0 big-title" id="about">About me !</h2>
+          <hr class="divider my-4">
+          <!-- <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p> -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 mr-auto text-center">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item danger">
+              <span style="font-size: 48px; color: #e67e22;"> <i class="fab fa-html5 "></i></span> 
+              <span style="font-size: 48px; color: Dodgerblue;"><i class="fab fa-css3-alt"></i></span>
+              <span style=" font-size: 48px; color: #f1c40f";><i class="fab fa-js"></i></span> <br>
+              <span class="techno "> HTML , CSS & JS </span>
+            </li>
+            <li class="list-group-item pl-0">
+              <span style=" font-size: 48px; color: rgb(91, 80, 155)";><i class="fab fa-php"></i></span>
+              <span style=" font-size: 48px; color: rgb(91, 80, 155)";><img src="img/mysql.png" width="52" height="52" alt="img-php"></span><br>
+              <span class="techno">  PHP & MySQL</span>
+            </li>
+            <li class="list-group-item danger">
+              <span style="font-size: 48px; color: black;"> <i class="fab fa-linux"></i></span> 
+              <span style="font-size: 48px; color: #ff6348;"><i class="fab fa-git-alt"></i></i></span> <br>
+              <span class="techno "> Linux & git </span>
+            </li>
+            <li class="list-group-item">
+               <span style=" font-size: 48px; color: rgb(91, 80, 155)";><i class="fab fa-symfony"></i></span>
+              <span style=" font-size: 48px; color: rgb(91, 80, 155)";><i class="fab fa-bootstrap"></i></span><br>
+              <span class="techno">Symfony & Bootstrap4</span>
+            </li>
+            <hr>
+          </ul>
+          <!-- <i class="fas fa-envelope fa-3x mb-3 text-muted"></i> -->
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <!-- <a class="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a> -->
+        </div>
+        <div class="col-lg-6 ml-auto text-center mb-5 mb-lg-0 clip-path row">
+          <img src="https://www.cadres-dirigeants.fr/wp-content/uploads/2017/12/femme-ordinateur.jpg" class="card-img" alt="...">
+          <div class="text-desc"> <p>I am a junior web developer, I live in Strasbourg. <br>
+            I have a great passion for user interface effects and creating dynamic projects, <br>
+            <span class="text-me">Let's do something special.</span> </p> </div></div>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section
+  <section class="page-section" id="projects">
+    <div class="container">
+      <h2 class="text-center mt-0">At Your Service</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Sturdy Themes</h3>
+            <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Up to Date</h3>
+            <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Ready to Publish</h3>
+            <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Made with Love</h3>
+            <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+
+   <!-- Portfolio Section -->
+  
+   <section id="portfolio">
+    <div class="container-fluid ">
+      <h2 class=" mt-0 big-title text-center "id="project">My projects</h2>
+      <hr class="divider  my-4 pb-5">
+      <div class="row no-gutters">
+        <div class="col-lg-6 col-sm-6 p-2">
+          <a class="portfolio-box" href="img/bomberman/bomb2.png">
+            <img class="img-fluid" src="img/bomberman/bomb1.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Bomberman
+              </div>
+              <div class="project-name">
+               +
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-6 col-sm-6 p-2">
+          <a class="portfolio-box" href="img/template-jeux/glob.png">
+            <img class="img-fluid" src="img/template-jeux/inscription.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Video game site
+              </div>
+              <div class="project-name">
+               +
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-6 col-sm-6 p-2">
+          <a class="portfolio-box" href="img/plugin2.png">
+            <img class="img-fluid" src="img/plugin.png" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Weather plugin
+              </div>
+              <div class="project-name">
+                +
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-6 col-sm-6 p-2">
+          <a class="portfolio-box" href="img/inu2.png">
+            <img class="img-fluid" src="img/inu.png" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Project of presentation of the student 
+              </div>
+              <div class="project-name">
+               +
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- CV Section -->
+  <section class="page-section ">
+    <div class="container text-center">
+      <h2 class="mb-4 text-cv ">For more information download my CV!</h2>
+      <a class="btn btn-xl btn-cv text-white" href="https://onedrive.live.com/?cid=0D5652A580EA0421&id=D5652A580EA0421%21116&parId=root&o=OneUp" target="_blank">Download Now!</a>
+    </div>
+  </section>
+
+ <!-- Contact Section -->
+ <section class="page-section global clip-contact " id="contact">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h2 class=" mt-0 big-title">Contact me</h2>
+        <hr class="divider  my-4">
+        <!-- ////////////////////////////////////// -->
+        <div class="contact-form">
+          <form action="/examples/actions/confirmation.php" method="post">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="inputName">Name</label>
+                  <input type="text" class="form-control" id="inputName" required>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="inputEmail">Email</label>
+                  <input type="email" class="form-control" name="email" id="inputEmail" required>
+                </div>
+              </div>
+            </div>            
+            <div class="form-group">
+              <label for="inputSubject">Subject</label>
+              <input type="text" class="form-control" id="inputSubject" required>
+            </div>
+            <div class="form-group">
+              <label for="inputMessage">Message</label>
+              <textarea class="form-control" id="inputMessage" rows="5" name="message" required></textarea>
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Send</button>
+            </div>            
+          </form>
+        </div>
+        <!-- ////////////////////////////////////// -->
+
+    
+      </div>
+    </div>
+  </div>
+  <div class="text-center">
+    <a href="#page-top" class="js-scroll-trigger"><span class="hand " style=" font-size: 50px; color: #e31b6d";><i class="far fa-hand-point-up"></i></a>
+</div> 
+</section>
+<?php
+    if (isset($_POST['message'])) {
+      $retour = mail('iderkenza1@gmail.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+      if ($retour) {
+          echo '<p>Votre message a bien été envoyé.</p>';
+      }
+    }  
+ 
+    // if (isset($_POST['message'])) {
+    //     $position_arobase = strpos($_POST['email'], '@');
+    //     if ($position_arobase === false)
+    //         echo '<p>Votre email doit comporter un arobase.</p>';
+    //     else {
+    //         $retour = mail('iderkenza1@gmail.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+    //         if($retour)
+    //             echo '<p>Votre message a été envoyé.</p>';
+    //         else
+    //             echo '<p>Erreur.</p>';
+    //     }
+    // }
+  
+      ?>
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy;<span class="date">2020</span>- KENZA GUETTAL</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
